bf8079ff53fc78f3aa704104001a349df9ad485f
jihyun-j <jihyun9142@gmail.com>
2024-12-20 17:09:22 +0900

    [refector] 페이지 라우터 수정

--- front/src/utils/AuthLayout.tsx
+++ front/src/utils/AuthLayout.tsx
@@ -1,17 +1,12 @@
-import { CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import { validateAccessToken } from "../../api/auth";
-import { useLoginStore } from "../../store/useLoginStore";
-import TokenRefreshAlert from "./TokenRefreshAlert";
-import { useTokenExpirationCheck } from "../../utils/checkTokenExpiration";
+import { useLoginStore } from "@/store/useLoginStore";
+import { validateAccessToken } from "@/api/auth";
 
 const AuthLayout = () => {
   const navigate = useNavigate();
   const { accessToken, isLoggedIn } = useLoginStore();
   const [isLoading, setIsLoading] = useState(true);
-
-  const showAlert = useTokenExpirationCheck(accessToken, 5 * 60 * 1000); // 만료 5분 전 알림
 
   useEffect(() => {
     const checkToken = async () => {
@@ -28,18 +23,11 @@ const AuthLayout = () => {
 
   if (isLoading) {
     return (
-      <div className="flex items-center justify-center min-h-screen">
-        <CircularProgress color="primary" size={150} />
-      </div>
+      <div className="flex items-center justify-center min-h-screen"></div>
     );
   }
 
-  return (
-    <>
-      <Outlet />
-      {showAlert && <TokenRefreshAlert open={showAlert} onClose={() => {}} />}
-    </>
-  );
+  return <Outlet />;
 };
 
 export default AuthLayout;
--- front/src/utils/PrivateRoute.tsx
+++ front/src/utils/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useLoginStore } from "../../store/useLoginStore";
+import { useLoginStore } from "../store/useLoginStore";
 
 interface PrivateRouteProps {
   children: JSX.Element;
