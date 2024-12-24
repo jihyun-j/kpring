4f3b00505ce6cc92418da0e5779520b31aaff03a
jihyun-j <jihyun9142@gmail.com>
2024-12-24 23:14:27 +0900

    [refector] api method 추가

--- front/src/api/axios/requests.ts
+++ front/src/api/axios/requests.ts
@@ -4,62 +4,109 @@ const SERVER_URL = "/server/api/v1";
 
 export const AUTH_API = {
   POST_REQUEST: {
-    accessToken: `${AUTH_URL}/access_token`,
-    token: `${AUTH_URL}/token`,
-    validation: `${AUTH_URL}/validation`,
+    accessToken: { url: `${AUTH_URL}/access_token`, method: "POST" },
+    token: { url: `${AUTH_URL}/token`, method: "POST" },
+    validation: { url: `${AUTH_URL}/validation`, method: "POST" },
   },
+
   DELETE_REQUEST: {
-    deleteToken: (jwtToken: string) => `${AUTH_URL}/token/${jwtToken}`,
+    deleteToken: (jwtToken: string) => ({
+      url: `${AUTH_URL}/token/${jwtToken}`,
+      method: "DELETE",
+    }),
   },
 };
 
 export const USER_API = {
   GET_REQUEST: {
-    fetchUsers: `${USER_URL}/user`,
-    fetchProfile: (userId: string) => `${USER_URL}/user/${userId}`,
-    fetchFriends: (userId: string) => `${USER_URL}/user/${userId}/friends`,
-    fetchFriendRequests: (userId: string) =>
-      `${USER_URL}/user/${userId}/requests`,
+    fetchUsers: { url: `${USER_URL}/user`, method: "GET" },
+    fetchProfile: (userId: string) => ({
+      url: `${USER_URL}/user/${userId}`,
+      method: "GET",
+    }),
+    fetchFriends: (userId: string) => ({
+      url: `${USER_URL}/user/${userId}/friends`,
+      method: "GET",
+    }),
+    fetchFriendRequests: (userId: string) => ({
+      url: `${USER_URL}/user/${userId}/requests`,
+      method: "GET",
+    }),
   },
+
   POST_REQUEST: {
-    login: `${USER_URL}/login`,
-    logout: `${USER_URL}/logout`,
-    join: `${USER_URL}/user`,
-    requestFriend: (userId: string, friendId: string) =>
-      `${USER_URL}/user/${userId}/friend/${friendId}`,
+    login: { url: `${USER_URL}/login`, method: "POST" },
+    logout: { url: `${USER_URL}/logout`, method: "POST" },
+    join: { url: `${USER_URL}/user`, method: "POST" },
+    requestFriend: (userId: string, friendId: string) => ({
+      url: `${USER_URL}/user/${userId}/friend/${friendId}`,
+      method: "POST",
+    }),
   },
+
   PATCH_REQUEST: {
-    editProfile: (userId: string) => `${USER_URL}/user/${userId}`,
-    acceptFriendRequest: (userId: string, friendId: string) =>
-      `${USER_URL}/user/${userId}/friend/${friendId}`,
+    editProfile: (userId: string) => ({
+      url: `${USER_URL}/user/${userId}`,
+      method: "PATCH",
+    }),
+    acceptFriendRequest: (userId: string, friendId: string) => ({
+      url: `${USER_URL}/user/${userId}/friend/${friendId}`,
+      method: "PATCH",
+    }),
   },
 
   DELETE_REQUEST: {
-    deleteAccount: (userId: string) => `${USER_URL}/user/${userId}`,
-    deleteFriend: (userId: string, friendId: string) =>
-      `${USER_URL}/user/${userId}/friend/${friendId}`,
+    deleteAccount: (userId: string) => ({
+      url: `${USER_URL}/user/${userId}`,
+      method: "DELETE",
+    }),
+    deleteFriend: (userId: string, friendId: string) => ({
+      url: `${USER_URL}/user/${userId}/friend/${friendId}`,
+      method: "DELETE",
+    }),
   },
 };
 
 export const SERVER_API = {
   GET_REQUEST: {
-    fetchServerCategories: `${SERVER_URL}/category`,
-    fetchServers: `${SERVER_URL}/server`,
-    fetchSingleServer: (serverId: string) => `${SERVER_URL}/server/${serverId}`,
+    fetchServerCategories: { url: `${SERVER_URL}/category`, method: "GET" },
+    fetchServers: { url: `${SERVER_URL}/server`, method: "GET" },
+    fetchSingleServer: (serverId: string) => ({
+      url: `${SERVER_URL}/server/${serverId}`,
+      method: "GET",
+    }),
   },
+
   POST_REQUEST: {
-    createServer: `${SERVER_URL}/server`,
+    createServer: { url: `${SERVER_URL}/server`, method: "POST" },
   },
+
   PUT_REQUEST: {
-    joinInServer: (serverId: string) => `${SERVER_URL}/server/${serverId}/user`,
-    inviteToServer: (serverId: string, userId: string) =>
-      `${SERVER_URL}/server/${serverId}/invitation/${userId}`,
+    joinInServer: (serverId: string) => ({
+      url: `${SERVER_URL}/server/${serverId}/user`,
+      method: "POST",
+    }),
+    inviteToServer: (serverId: string, userId: string) => ({
+      url: `${SERVER_URL}/server/${serverId}/invitation/${userId}`,
+      method: "POST",
+    }),
   },
+
   PATCH_REQUEST: {
-    inheritHostRole: (serverId: string) => `${SERVER_URL}/server/${serverId}`,
+    inheritHostRole: (serverId: string) => ({
+      url: `${SERVER_URL}/server/${serverId}`,
+      method: "PATCH",
+    }),
   },
+
   DELETE_REQUEST: {
-    deleteServer: (serverId: string) => `${SERVER_URL}/server/${serverId}`,
-    leaveServer: (serverId: string) => `${SERVER_URL}/server/${serverId}/exit`,
+    deleteServer: (serverId: string) => ({
+      url: `${SERVER_URL}/server/${serverId}`,
+      method: "DELETE",
+    }),
+    leaveServer: (serverId: string) => ({
+      url: `${SERVER_URL}/server/${serverId}/exit`,
+      method: "DELETE",
+    }),
   },
 };
