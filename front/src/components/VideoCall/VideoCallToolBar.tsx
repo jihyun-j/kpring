73ca3cb537e080f1b28d825d82200560851b5379
jihyun-j <jihyun9142@gmail.com>
2024-12-24 22:23:43 +0900

    [chore] Video 관련 컴포넌트에서 MUI 제거

--- front/src/components/VideoCall/VideoCallToolBar.tsx
+++ front/src/components/VideoCall/VideoCallToolBar.tsx
@@ -1,22 +1,20 @@
 // TODO : 비디오 툴바
-import { BsArrowDown } from "react-icons/bs";
+import { BsArrowDown, BsShare, BsWindowDesktop } from "react-icons/bs";
 import { HiCamera } from "react-icons/hi";
-import { IoMic } from "react-icons/io5";
-import { RiScreenshotFill } from "react-icons/ri";
+import { IoMic, IoSearchCircleOutline } from "react-icons/io5";
+import { RiScreenshotFill, RiStopCircleLine } from "react-icons/ri";
 
 const VideoCallToolBar = () => {
   return (
     <div className="bg-white w-52 h-12 rounded-3xl flex items-center justify-around px-2">
       <div className="pointer">
         <IoMic />
-        <BsArrowDown className="rounded-sm" />
       </div>
       <div className="pointer">
         <HiCamera />
-        <BsArrowDown className="rounded-sm" />
       </div>
       <div>
-        <RiScreenshotFill />
+        <BsWindowDesktop />
       </div>
     </div>
   );
