e5d5f080c6a2f659ca95b4c6c02dfd079c020f28
jihyun-j <jihyun9142@gmail.com>
2024-12-24 23:20:24 +0900

    [feat] Templat: 회원가입 및 로그인 템플릿 추가

--- front/src/components/templates/LoginTemplate.tsx
+++ front/src/components/templates/LoginTemplate.tsx
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router";
 import LoginForm from "../organisms/LoginForm";
 import Button from "../atoms/Button";
+import Divider from "../atoms/Divider";
 
 const LoginTemplate = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <LoginForm />
-      <Button onClick={() => navigate("/join")} color="red">
+      <Divider />
+      <span>계정이 없으신가요?</span>
+      <Button onClick={() => navigate("/join")} color="bg-dark">
         회원가입
       </Button>
     </div>
