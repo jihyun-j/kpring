da7c364f13cb332f707904e8f1bc0bdf31c296b7
jihyun-j <jihyun9142@gmail.com>
2024-12-24 23:21:03 +0900

    [feat] Page: 회원가입 및 로그인 페이지 생성

--- front/src/components/pages/Login.tsx
+++ front/src/components/pages/Login.tsx
@@ -1,7 +1,15 @@
+import Text from "../atoms/Text";
 import LoginTemplate from "../templates/LoginTemplate";
 
 const Login = () => {
-  return <LoginTemplate />;
+  return (
+    <div className="h-svh flex flex-col items-center justify-center">
+      <Text color="text-dark" size="">
+        로그인
+      </Text>
+      <LoginTemplate />
+    </div>
+  );
 };
 
 export default Login;
